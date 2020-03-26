40383d783c04974eac8315921e6433d6e800d4a3
Breno Costa <breno.costa182@gmail.com>
2020-03-26 20:22:17 -0300

    Melhorando sistema de autenticaçao de usuários. Adicionando novas urls para deletar, pagar e atualizar pedidos. Consertando função de atualização de produto.

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,13 @@ from api.views import cria_novo_produto
 from api.views import listar_detalhes_do_produto
 from api.views import deletar_produto
 from api.views import altera_produto
-from api.views import retorna_todos_os_pedidos
+from api.views import retorna_todos_os_pedidos_do_usuario
 from api.views import cria_novo_pedido
 from api.views import deletar_pedido
 from api.views import loginNoSistema
+from api.views import detalhes_do_pedido
+from api.views import alterar_pedido
+from api.views import pagar_pedido
 
 app_name = 'api'
 
@@ -18,7 +21,10 @@ urlpatterns = [
     path('detalhesDoProduto/<int:idDoProduto>', listar_detalhes_do_produto, name='detalhesDoProduto'),
     path('deletarProduto/<int:idDoProduto>', deletar_produto, name='deletarDoProduto'),
     path('atualizaProduto/<int:idDoProduto>', altera_produto, name='atualizaProduto'),
-    path('listaPedidos/', retorna_todos_os_pedidos, name='listaPedidos'),
+    path('listaPedidos/', retorna_todos_os_pedidos_do_usuario, name='listaPedidos'),
     path('criaPedido/', cria_novo_pedido, name='criaPedido'),
     path('deletarPedido/<int:idDoPedido>', deletar_pedido, name='deletarPedido'),
+    path('detalhesDoPedido/<int:idDoPedido>', detalhes_do_pedido, name='detalharPedido'),
+    path('alterarPedido/<int:idDoPedido>', alterar_pedido, name='alterarPedido'),
+    path('pagarPedido/<int:idDoPedido>', pagar_pedido, name='pagarPedido'),   
 ]
--- api/views.py
+++ api/views.py
@@ -11,17 +11,42 @@ from api.serializers import PedidosSerializer
 
 token_userID = dict([])
 
+def usuarioJaLogado(idDoUsuario):
+    return idDoUsuario in token_userID.values()
+
+def autenticar(headers):
+        try:
+            idUsusario = token_userID[headers['token']]
+            return idUsusario
+        except:
+            return 0
+
+def decrementaEstoqueDoProduto(idDoProduto, quantidadeASerDecrementada):
+    produto = Produto.objects.get(id=idDoProduto)
+    if produto.estoque >= quantidadeASerDecrementada:
+        produto.estoque = produto.estoque-quantidadeASerDecrementada
+        produto.save()
+    else:
+        raise ValueError('Valor requerido maior que o estoque')
+
+
+def incrementaEstoqueDoProduto(idDoProduto, quantidadeASerIncrementada):
+    produto = Produto.objects.get(id=idDoProduto)
+    produto.estoque = produto.estoque+quantidadeASerIncrementada
+    produto.save()
+
 @api_view(['POST',  ])
 def loginNoSistema(request):
-    usuario = Usuario.objects.filter(nomeDeUsuario=request.data['nomeUsuario'], senha=request.data['senha'])[0]
-    if usuario:
-        token=secrets.token_hex(16)
-        token_userID[token] = usuario.id
-        return Response(token,status=status.HTTP_200_OK)
-    else:
-        return Response(token,status=status.HTTP_404_NOT_FOUND)
-
-
+    try:
+        usuario = Usuario.objects.filter(nomeDeUsuario=request.data['nomeUsuario'], senha=request.data['senha'])[0]
+        if usuarioJaLogado(usuario.id)==False:
+            token=secrets.token_hex(16)
+            token_userID[token] = usuario.id
+            return Response(token,status=status.HTTP_200_OK)
+        else:
+            return Response(status.HTTP_208_ALREADY_REPORTED)
+    except IndexError:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['GET',  ])
 def retorna_todos_os_produtos(request):
@@ -35,56 +60,115 @@ def listar_detalhes_do_produto(request, idDoProduto):
 
 @api_view(['POST',  ])
 def cria_novo_produto(request):
-    produtoSerializado = ProdutoSerializer(data=request.data)
-    if produtoSerializado.is_valid():
-        produtoSerializado.save()
-        return Response(produtoSerializado.data, status=status.HTTP_201_CREATED) 
-    return Response(produtoSerializado.errors, status=status.HTTP_400_BAD_REQUEST)
+    if autenticar(request.headers):
+        produtoSerializado = ProdutoSerializer(data=request.data)
+        if produtoSerializado.is_valid():
+            produtoSerializado.save()
+            return Response(produtoSerializado.data, status=status.HTTP_201_CREATED) 
+        return Response(produtoSerializado.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
 
 @api_view(['DELETE',  ])
 def deletar_produto(request, idDoProduto):
-    try: 
-        Produto.objects.get(id=idDoProduto).delete()
-        return Response(status=status.HTTP_200_OK)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    if autenticar(request.headers):
+        try: 
+            Produto.objects.get(id=idDoProduto).delete()
+            return Response(status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-#TOFIX 
 @api_view(['PATCH',  ])
 def altera_produto(request, idDoProduto):
-    produtoSerializado = ProdutoSerializer(Produto, data=request.data)
-    if produtoSerializado.is_valid():
-        instanciaDoProduto = Produto.objects.get(id=idDoProduto)
-        produtoSerializado.update(instanciaDoProduto, produtoSerializado.data)
-        return Response(produtoSerializado.data, status=status.HTTP_201_CREATED) 
-    return Response(produtoSerializado.errors, status=status.HTTP_400_BAD_REQUEST)
+    if autenticar(request.headers):
+        produtoASerAtualizado=Produto.objects.get(id=idDoProduto)
+        try:
+            produtoASerAtualizado.nome = request.data['nome']
+            produtoASerAtualizado.descricao = request.data['descricao']
+            produtoASerAtualizado.preco = request.data['preco']
+            produtoASerAtualizado.estoque = request.data['estoque']
+            produtoASerAtualizado.save()
+            return Response(ProdutoSerializer(produtoASerAtualizado).data, status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 @api_view(['GET',  ])
-def retorna_todos_os_pedidos(request):
-    pedidos = Pedidos.objects.all()
-    pedidosSerializado = PedidosSerializer(pedidos, many=True)
-    return Response(pedidosSerializado.data, status=status.HTTP_200_OK)
+def retorna_todos_os_pedidos_do_usuario(request):
+    idDoUsuario = autenticar(request.headers)
+    if idDoUsuario:
+        pedidos = Pedidos.objects.filter(usuario_id=idDoUsuario)
+        pedidosSerializado = PedidosSerializer(pedidos, many=True)
+        return Response(pedidosSerializado.data, status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_401_UNAUTHORIZED)
+
 
 @api_view(['POST',  ])
 def cria_novo_pedido(request):
-    pedidoSerializado = PedidosSerializer(data=request.data)
-    if pedidoSerializado.is_valid():
-        pedidoSerializado.save()
-        return Response(pedidoSerializado.data, status=status.HTTP_201_CREATED) 
-    return Response(pedidoSerializado.errors, status=status.HTTP_400_BAD_REQUEST)
+    idDoUsuario = autenticar(request.headers)
+    if idDoUsuario:
+        decrementaEstoqueDoProduto(request.data['produto'],request.data['quantidadeDeItens'])
+        pedido = Pedidos()
+        pedido.usuario=Usuario.objects.get(id=idDoUsuario)
+        pedido.produto=Produto.objects.get(id=request.data['produto'])
+        pedido.quantidadeDeItens=request.data['quantidadeDeItens']
+        pedido.precoTotal = pedido.produto.preco * pedido.quantidadeDeItens
+        pedido.pago = False
+        pedido.save()
+        return Response(PedidosSerializer(pedido).data, status=status.HTTP_201_CREATED)   
+    return Response(status=status.HTTP_401_UNAUTHORIZED)
+
 
 @api_view(['DELETE',  ])
 def deletar_pedido(request, idDoPedido):
-    try: 
-        Pedidos.objects.get(id=idDoPedido).delete()
-        return Response(status=status.HTTP_200_OK)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    idDoUsuario = autenticar(request.headers)
+    if idDoUsuario:
+        try: 
+            Pedidos.objects.filter(id=idDoPedido, usuario = idDoUsuario)[0].delete()
+            return Response(status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    return Response(status=status.HTTP_401_UNAUTHORIZED)
+
 
 @api_view(['GET',  ])
-def listar_detalhes_do_pedido(request, idDoPedido):
-    try: 
-        pedidoSerializado=PedidosSerializer(Pedidos.objects.get(id=idDoPedido)).data    
-        return Response(pedidoSerializado, status=status.HTTP_200_OK)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+def detalhes_do_pedido(request, idDoPedido):
+    idDoUsuario = autenticar(request.headers)
+    if idDoUsuario:
+        try: 
+            pedidoSerializado=PedidosSerializer(Pedidos.objects.filter(id=idDoPedido, usuario = idDoUsuario)[0]).data    
+            return Response(pedidoSerializado, status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view(['PATCH',  ])
+def alterar_pedido(request, idDoPedido):
+    idDoUsuario = autenticar(request.headers)
+    if idDoUsuario:
+        pedido = Pedidos.objects.filter(id=idDoPedido, usuario = idDoUsuario)[0]
+        if pedido.quantidadeDeItens < request.data['quantidadeDeItens']:
+            decrementaEstoqueDoProduto(pedido.produto.id, request.data['quantidadeDeItens'] - pedido.quantidadeDeItens)
+            pedido.quantidadeDeItens = request.data['quantidadeDeItens']
+            pedido.precoTotal = pedido.produto.preco * pedido.quantidadeDeItens 
+            pedido.save()
+            return Response(PedidosSerializer(pedido).data, status=status.HTTP_200_OK)
+        else:
+            incrementaEstoqueDoProduto(pedido.produto.id, pedido.quantidadeDeItens - request.data['quantidadeDeItens'])
+            pedido.quantidadeDeItens = request.data['quantidadeDeItens']
+            pedido.precoTotal = pedido.produto.preco * pedido.quantidadeDeItens 
+            pedido.save()
+            return Response(PedidosSerializer(pedido).data, status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['PATCH',  ])
+def pagar_pedido(request, idDoPedido):
+    idDoUsuario = autenticar(request.headers)
+    if idDoUsuario:
+        pedido = Pedidos.objects.filter(id=idDoPedido, usuario = idDoUsuario)[0]    
+        pedido.pago = True
+        pedido.save()
+        return Response(PedidosSerializer(pedido).data, status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_401_UNAUTHORIZED)
